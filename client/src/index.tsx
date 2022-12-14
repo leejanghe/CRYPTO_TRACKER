5470ee387bc76a7eb683a713a65afd67c0190d1d
leejanghe <babybye777@gmail.com>
2022-12-15 01:06:26 +0900

    theme style

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { darkTheme, lightTheme } from "./theme";
+import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <ThemeProvider theme={darkTheme}>
+  <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
 );
