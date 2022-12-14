b70572151eb2d921e50baca97487a495c9845bb0
leejanghe <babybye777@gmail.com>
2022-12-15 02:39:45 +0900

    Route states

--- client/src/router/Coin.tsx
+++ client/src/router/Coin.tsx
@@ -1,16 +1,49 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
 
-interface RouterParams {
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface RouteParams {
   coinId: string;
+}
+interface RouteState {
+  name: string;
 }
 
 function Coin() {
-  const { coinId } = useParams<RouterParams>();
+  const [loading, setLoading] = useState(true);
+  const { coinId } = useParams<RouteParams>();
   console.log(coinId);
+  const { state } = useLocation<RouteState>();
   return (
-    <div>
-      <h1>Coin {coinId}</h1>
-    </div>
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
   );
 }
 
