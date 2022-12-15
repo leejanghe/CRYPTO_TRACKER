50eae27a871de8dabd493042edc1798ed37301a5
leejanghe <babybye777@gmail.com>
2022-12-16 01:15:20 +0900

    react query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { theme } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
